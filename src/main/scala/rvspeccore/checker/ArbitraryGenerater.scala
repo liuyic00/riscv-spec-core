a6a7fe1a5a9f2a9d673840d76d39f0ba991e7eb5
liuyic00 <liuyic00@gmail.com>
2025-01-06 16:59:54 +0800

    modi

--- src/main/scala/rvspeccore/checker/ArbitraryGenerater.scala
+++ src/main/scala/rvspeccore/checker/ArbitraryGenerater.scala
@@ -12,7 +12,7 @@ object ArbitraryRegFile {
     BoringUtils.addSink(initval, uniqueIdArbitraryRegFile)
     initval
   }
-  def init(implicit XLEN: Int): Vec[UInt] = {
+  def init(implicit XLEN: Int):Vec[UInt] = {
     val rf = Wire(Vec(32, UInt(XLEN.W)))
     rf.map(_ := DontCare)
     rf(0) := 0.U
